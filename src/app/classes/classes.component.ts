1f8d12dc65e27339311dcac8295676804cda8fa7
Sabrina Seibel <seibelsabrina@gmail.com>
2018-03-03 05:29:41 -0500

    css stuff for classes

--- src/app/classes/classes.component.ts
+++ src/app/classes/classes.component.ts
@@ -6,6 +6,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./classes.component.css']
 })
 export class ClassesComponent implements OnInit {
+  classes: any[] = [
+    {
+      code: 'CS2340',
+      name: 'Objects and Design',
+      professor: 'Bob Waters',
+      semester: 'Spring 2018',
+      section: 'B',
+      time: ['MWF 14:00-15:00']
+    },
+    {
+      code: 'CS1332',
+      name: 'Data Structures and Algorithms',
+      professor: 'Marry Hudachek',
+      semester: 'Spring 2018',
+      section: 'A',
+      time: ['MWF 9:00-10:00']
+    },
+    {
+      code: 'CS1332',
+      name: 'Data Structures and Algorithms',
+      professor: 'Marry Hudachek',
+      semester: 'Spring 2018',
+      section: 'A',
+      time: ['MWF 9:00-10:00']
+    },
+    {
+      code: 'CS1332',
+      name: 'Data Structures and Algorithms',
+      professor: 'Marry Hudachek',
+      semester: 'Spring 2018',
+      section: 'A',
+      time: ['MWF 9:00-10:00']
+    },
+    {
+      code: 'CS1332',
+      name: 'Data Structures and Algorithms',
+      professor: 'Marry Hudachek',
+      semester: 'Spring 2018',
+      section: 'A',
+      time: ['MWF 9:00-10:00']
+    }
+  ];
 
   constructor() { }
 
