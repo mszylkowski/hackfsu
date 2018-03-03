276da146ffc34039bf2de910fff0ac61df167e96
Matias Szylkowski <mszylkowski@hotmail.com>
2018-03-03 05:39:38 -0500

    download classes from firebase

--- src/app/classes/classes.component.ts
+++ src/app/classes/classes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase } from 'angularfire2/database';
 
 @Component({
   selector: 'app-classes',
@@ -6,50 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./classes.component.css']
 })
 export class ClassesComponent implements OnInit {
-  classes: any[] = [
-    {
-      code: 'CS2340',
-      name: 'Objects and Design',
-      professor: 'Bob Waters',
-      semester: 'Spring 2018',
-      section: 'B',
-      time: ['MWF 14:00-15:00']
-    },
-    {
-      code: 'CS1332',
-      name: 'Data Structures and Algorithms',
-      professor: 'Marry Hudachek',
-      semester: 'Spring 2018',
-      section: 'A',
-      time: ['MWF 9:00-10:00']
-    },
-    {
-      code: 'CS1332',
-      name: 'Data Structures and Algorithms',
-      professor: 'Marry Hudachek',
-      semester: 'Spring 2018',
-      section: 'A',
-      time: ['MWF 9:00-10:00']
-    },
-    {
-      code: 'CS1332',
-      name: 'Data Structures and Algorithms',
-      professor: 'Marry Hudachek',
-      semester: 'Spring 2018',
-      section: 'A',
-      time: ['MWF 9:00-10:00']
-    },
-    {
-      code: 'CS1332',
-      name: 'Data Structures and Algorithms',
-      professor: 'Marry Hudachek',
-      semester: 'Spring 2018',
-      section: 'A',
-      time: ['MWF 9:00-10:00']
-    }
-  ];
+  classes: any[] = [];
 
-  constructor() { }
+  constructor(private db: AngularFireDatabase) {
+    db.list('classes').valueChanges().subscribe((e) => {
+      this.classes = e;
+    });
+  }
 
   ngOnInit() {
   }
